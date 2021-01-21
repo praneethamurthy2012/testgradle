b7e5ebe68e4d3dc235522c6977fc05278ce00b12
Sreedharamurthy n <sreedharamurthy.n@wibmodc.com>
2021-01-21 18:54:19 +0530

    Initial commit

---
+++ src/main/java/com/elk/ELK/Demo/api/WelcomeApi.java
@@ -0,0 +1,15 @@
+package com.elk.ELK.Demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeApi {
+	
+	@GetMapping(value="/api/welcome")
+	public String welcome() {
+		
+		return "Welcome to Spring Boot ELK session";
+	}
+
+}
---
+++ src/test/java/com/elk/ELK/Demo/ElkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elk.ELK.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
