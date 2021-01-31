326e5beac995dbced587d3960f2d18684cf12934
sreedharamurthy.n <sreedharamurthy.n@WIB-LAP-003752.wibmodc.com>
2021-01-31 16:15:56 +0530

    Added Promotion code and Junit test case examples

--- src/main/java/com/course/practicaljava/server/CarApi.java
+++ src/main/java/com/course/practicaljava/server/CarApi.java
@@ -1,10 +1,12 @@
 package com.course.practicaljava.server;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +28,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.course.practicaljava.api.response.ErrorResponse;
 import com.course.practicaljava.entity.Car;
+import com.course.practicaljava.exception.IllegalApiParamException;
 import com.course.practicaljava.repository.CarElasticRepository;
 import com.course.practicaljava.service.CarService;
 
@@ -87,15 +95,38 @@ public class CarApi {
 	}
 	
 	@GetMapping(value = "/cars/{brand}/{color}")
-	public List<Car> findCarsByPath(@PathVariable String brand, @PathVariable String color, @RequestParam(defaultValue = "0") int page,
+	public ResponseEntity<Object> findCarsByPath(@PathVariable String brand, @PathVariable String color, @RequestParam(defaultValue = "0") int page,
 			@RequestParam(defaultValue = "10") int size) {
+		
+		var headers = new HttpHeaders();
+		headers.add(HttpHeaders.SERVER, "Spring");
+		headers.add("X-Custom-Header", "Custom Response Header");
+		
+		if (StringUtils.isNumeric(color)) {
+			var errorResponse = new ErrorResponse("Invalid color : " + color, LocalDateTime.now());
+
+			return new ResponseEntity<Object>(errorResponse, headers, HttpStatus.BAD_REQUEST);
+		}
+		
+		
 		var pageable = PageRequest.of(page, size);
-		return carRepository.findByBrandAndColor(brand, color, pageable).getContent();
+		var cars = carRepository.findByBrandAndColor(brand, color, pageable).getContent();
+		
+		return ResponseEntity.ok().headers(headers).body(cars);
 	}
 	
 	@GetMapping(value = "/cars")
 	public List<Car> findCarsByParam(@RequestParam String brand, @RequestParam String color, @RequestParam(defaultValue = "0") int page,
 			@RequestParam(defaultValue = "10") int size) {
+		
+		if (StringUtils.isNumeric(color)) {
+			throw new IllegalArgumentException("Invalid color : " + color);
+		}
+
+		if (StringUtils.isNumeric(brand)) {
+			throw new IllegalApiParamException("Invalid brand : " + brand);
+		}
+		
 		var pageable = PageRequest.of(page, size);
 		return carRepository.findByBrandAndColor(brand, color, pageable).getContent();
 	}
@@ -106,4 +137,15 @@ public class CarApi {
 		return carRepository.findByFirstReleaseDateAfter(firstReleaseDate);
 	}
 	
+	@ExceptionHandler(value = IllegalArgumentException.class)
+	private ResponseEntity<ErrorResponse> handleInvalidColorException(IllegalArgumentException e) {
+		var message = "Exception, " + e.getMessage();
+		LOG.warn(message);
+
+		var errorResponse = new ErrorResponse(message, LocalDateTime.now());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+	}
+	
+		
 }
---
+++ src/main/java/com/course/practicaljava/server/DefaultRestApi.java
@@ -0,0 +1,42 @@
+package com.course.practicaljava.server;
+
+import java.time.LocalTime;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class DefaultRestApi {
+
+	private Logger LOG = LoggerFactory.getLogger(DefaultRestApi.class);
+
+	@GetMapping(value = "/welcome")
+	public String welcome() {
+		LOG.info(StringUtils.join("Hello", " this is", " Spring boot", " REST API"));
+		return "Welcome to Spring Boot";
+	}
+
+	@GetMapping(value = "/time")
+	public String time() {
+		return LocalTime.now().toString();
+	}
+
+	@GetMapping(value = "/header-one")
+	public String headerByAnnotation(@RequestHeader(name = "User-agent") String headerUserAgent,
+			@RequestHeader(name = "Practical-java", required = false) String headerPracticalJava) {
+		return "User-agent : " + headerUserAgent + ", Practical-java : " + headerPracticalJava;
+	}
+	
+	@GetMapping(value = "/header-two")
+	public String headerByRequest(ServerHttpRequest request) {
+		return "User-agent : " + request.getHeaders().getValuesAsList("User-agent") + ", Practical-java : "
+				+ request.getHeaders().getValuesAsList("Practical-java");
+	}
+}
